82c23b1b3c3230846a797e3aac8398533d2f25bd
Jeong Yong-uk <ywj1022@gmail.com>
2014-08-11 00:23:23 +0900

    midigen.py with simple test code

---
+++ midigen.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+import simplejson as json
+import sys
+
+def main():
+    if len(sys.argv) is 1:
+        return
+    track_descriptor = sys.argv[1]
+
+if __name__ == "__main__":
+    main()
